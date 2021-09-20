474e137a6a1b9fef932eb0db727892d6e25b89d7
Damian Szczerba <shadipoppin17@gmail.com>
2021-09-20 20:07:48 +0200

    Fix wrong test case name

--- 02_assertion_methods/02_assertAlmostEqual.py
+++ 02_assertion_methods/02_assertAlmostEqual.py
@@ -14,5 +14,5 @@ class SimpleTest(unittest.TestCase):
     def test_case_04(self):
         self.assertAlmostEqual(0.12345678, 0.12345679)
 
-    def test_case_04(self):
+    def test_case_05(self):
         self.assertAlmostEqual(0.12345674, 0.12345679, 6)
