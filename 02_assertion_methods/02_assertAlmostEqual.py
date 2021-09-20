17200344e27cd89293ad5d2090f93a02c544fd41
Damian Szczerba <shadipoppin17@gmail.com>
2021-09-20 20:07:15 +0200

    Add 5 examples

---
+++ 02_assertion_methods/02_assertAlmostEqual.py
@@ -0,0 +1,18 @@
+import unittest
+
+class SimpleTest(unittest.TestCase):
+
+    def test_case_01(self):
+        self.assertEqual(0.1 + 0.2, 0.3)
+
+    def test_case_02(self):
+        self.assertAlmostEqual(0.1 + 0.2, 0.3)   
+
+    def test_case_03(self):
+        self.assertAlmostEqual(0.1234567, 0.1234567)
+
+    def test_case_04(self):
+        self.assertAlmostEqual(0.12345678, 0.12345679)
+
+    def test_case_04(self):
+        self.assertAlmostEqual(0.12345674, 0.12345679, 6)
